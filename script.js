221aa95a354a377a43bcde0ebe82bb57a0020ab5
Norman Trujillo <natrujillo9@gmail.com>
2024-03-12 21:16:06 -0400

    Add palindrome checking functionality to button click event handler

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const checkPalindrome = (input) => {
     alert("Please input a value");
     return;
   }
+
+  // Remove the previous result
+  result.replaceChildren();
+
+  const lowercaseInput = input.toLowerCase();
+
+  const reversedInput = lowercaseInput.split("").reverse().join("");
+
+  // Check if the reversed input is equal to the original input
+  if (lowercaseInput === reversedInput) {
+    result.textContent = `${input} is a palindrome`;
+  } else {
+    result.textContent = `${input} is not a palindrome`;
+  }
+
+  // Show the result.
+  result.classList.remove("hidden");
 };
 
 button.addEventListener("click", () => {
